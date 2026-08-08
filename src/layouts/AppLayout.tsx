959054cd5c7dd978d738bea0b9599c4b97782cc2
Douglas R <douglascorreia17@gmail.com>
2026-08-08 16:01:10 -0300

    STYLE: Ajustado para o header ser fixo e ter um menu suspenso.

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,6 +1,8 @@
 import { Link, Outlet } from 'react-router-dom'
 import { useState } from 'react'
 
+import { Menu, X } from 'lucide-react';
+
 import { useAuthStore } from '@/stores/useAuthStore'
 
 export default function AppLayout() {
@@ -19,85 +21,69 @@ export default function AppLayout() {
   
   return (
     <div className="min-h-screen bg-muted/30">
-      <header className="sticky top-0 border-b border-gray-200 bg-background py-4 z-10">
-        <div className="container flex items-center justify-end md:justify-between">
-          <nav className="hidden lg:block">
-            <ul className="flex gap-4 text-xs text-gray-600 [&>li>*]:font-semibold [&>li>*]:transition-colors [&>li>*]:hover:text-chateau-green-400">
-                <li>
-                  <Link onClick={closeMenu} className="block cursor-pointer" to="/dashboard">Dashboard</Link>
-                </li>
+      <header className="fixed md:sticky top-0 w-full border-b border-gray-200 bg-background py-4 z-10">
+        <div className="container flex items-center justify-between">
+          <div 
+            className="cursor-pointer"
+            onClick={toggleSubmenu}
+          >
+            <Menu />
+          </div>
 
-                <li>
-                  <Link onClick={closeMenu} className="block cursor-pointer" to="/categories">Categoria das despesas</Link>
-                </li>
+          <div 
+            className={`fixed top-0 left-0 w-full h-full max-w-96 bg-white border-r-2 border-r-gray-200 z-10 p-4
+              transform transition-transform duration-300 ease-in-out
+              ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'}
+            `}
+          >
+            <div className="flex items-center justify-between mb-4">
+              <span className="block font-semibold">Bem vindo, { profile?.nome }</span>
 
-                <li>
-                  <Link onClick={closeMenu} className="block cursor-pointer" to="/expenses">Despesas</Link>
-                </li>
+              <span
+                className="cursor-pointer"
+                onClick={toggleSubmenu}
+              >
+                <X />
+              </span>
+            </div>
 
-                <li>
-                  <Link onClick={closeMenu} className="block cursor-pointer" to="/budget">Salário</Link>
-                </li>
+            <nav>
+              <ul className="flex flex-col gap-2 text-xs text-gray-600 [&>li>*]:font-normal [&>li>*]:transition-colors [&>li>*]:hover:text-chateau-green-400">
+                  <li>
+                    <Link onClick={closeMenu} className="block cursor-pointer" to="/dashboard">Dashboard</Link>
+                  </li>
 
-                <li>
-                  <Link onClick={closeMenu} className="block cursor-pointer" to="/profile">Editar perfil</Link>
-                </li>
+                  <li>
+                    <Link onClick={closeMenu} className="block cursor-pointer" to="/categories">Categoria das despesas</Link>
+                  </li>
 
-                <li>
-                  <span className="cursor-pointer block" onClick={() => signOut()}>
-                    Sair
-                  </span>
-                </li>
-              </ul>
-          </nav>
+                  <li>
+                    <Link onClick={closeMenu} className="block cursor-pointer" to="/expenses">Despesas</Link>
+                  </li>
+
+                  <li>
+                    <Link onClick={closeMenu} className="block cursor-pointer" to="/budget">Salário</Link>
+                  </li>
+
+                  <li>
+                    <Link onClick={closeMenu} className="block cursor-pointer" to="/profile">Editar perfil</Link>
+                  </li>
+
+                  <li>
+                    <span className="cursor-pointer block" onClick={() => signOut()}>
+                      Sair
+                    </span>
+                  </li>
+                </ul>
+            </nav>
+          </div>
 
           <div className="relative group">
             <p
               className="flex items-center justify-center gap-4 bg-chateau-green-400 rounded-full size-9 text-white font-bold cursor-pointer"
-              onClick={toggleSubmenu}
-              aria-expanded={isMenuOpen}
-              aria-controls="profile-menu"
             >
               { profile?.nome.charAt(0) }
             </p>
-
-            <div
-              className={`absolute right-0 top-full z-50 mt-2 min-w-64 rounded-2xl border border-chateau-green-400 bg-white p-4 text-xs shadow-lg ${
-                isMenuOpen ? 'block' : 'hidden'
-              }`}
-            >
-              <p className="text-sm text-gray-700 font-semibold">Bem vindo, { profile?.nome.split(" ")[0] }!</p>
-
-              <nav className="mt-4">
-                <ul className="flex flex-col gap-2 text-xs text-gray-600 [&>li>*]:transition-colors [&>li>*]:hover:text-chateau-green-400">
-                    <li>
-                      <Link onClick={closeMenu} className="block cursor-pointer" to="/dashboard">Dashboard</Link>
-                    </li>
-
-                    <li>
-                      <Link onClick={closeMenu} className="block cursor-pointer" to="/categories">Categoria das despesas</Link>
-                    </li>
-
-                    <li>
-                      <Link onClick={closeMenu} className="block cursor-pointer" to="/expenses">Despesas</Link>
-                    </li>
-
-                    <li>
-                      <Link onClick={closeMenu} className="block cursor-pointer" to="/budget">Salário</Link>
-                    </li>
-
-                    <li>
-                      <Link onClick={closeMenu} className="block cursor-pointer" to="/profile">Editar perfil</Link>
-                    </li>
-
-                    <li>
-                      <span className="cursor-pointer block" onClick={() => signOut()}>
-                        Sair
-                      </span>
-                    </li>
-                  </ul>
-              </nav>
-            </div>
           </div>
         </div>
       </header>
